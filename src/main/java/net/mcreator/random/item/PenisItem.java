1abd784f861bba5adfdb0ed61eabf328132e8e5e
PotatoKingOfGods <Usuario@DESKTOP-IP2CC5U>
2025-01-22 22:57:58 -0300

    penis

---
+++ src/main/java/net/mcreator/random/item/PenisItem.java
@@ -0,0 +1,11 @@
+
+package net.mcreator.random.item;
+
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.Item;
+
+public class PenisItem extends Item {
+	public PenisItem() {
+		super(new Item.Properties().stacksTo(64).rarity(Rarity.COMMON));
+	}
+}
