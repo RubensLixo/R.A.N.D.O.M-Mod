4a49366a369975dd9cc029790e1ee6814ef4a646
PotatoKingOfGods <Usuario@DESKTOP-IP2CC5U>
2025-01-22 22:39:21 -0300

    borracha e random core

--- src/main/java/net/mcreator/random/item/RandomPart2Item.java
+++ src/main/java/net/mcreator/random/item/RandomPart2Item.java
@@ -1,6 +1,9 @@
 
 package net.mcreator.random.item;
 
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.Item;
+
 public class RandomPart2Item extends Item {
 	public RandomPart2Item() {
 		super(new Item.Properties().stacksTo(1).rarity(Rarity.EPIC));
--- src/main/java/net/mcreator/random/procedures/DiyRightclickedProcedure.java
+++ src/main/java/net/mcreator/random/procedures/DiyRightclickedProcedure.java
@@ -1,6 +1,19 @@
 package net.mcreator.random.procedures;
 
-import net.neoforged.bus.api.Event;
+import net.minecraft.world.phys.Vec3;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.entity.projectile.Projectile;
+import net.minecraft.world.entity.projectile.Arrow;
+import net.minecraft.world.entity.projectile.AbstractArrow;
+import net.minecraft.world.entity.ai.attributes.Attributes;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.ResourceKey;
+import net.minecraft.core.registries.Registries;
 
 public class DiyRightclickedProcedure {
 	public static void execute(LevelAccessor world, Entity entity) {
@@ -12,7 +25,7 @@ public class DiyRightclickedProcedure {
 				Level projectileLevel = _shootFrom.level();
 				if (!projectileLevel.isClientSide()) {
 					Projectile _entityToSpawn = new Object() {
-						public Projectile getArrow(Level level, Entity shooter, float damage, int knockback, byte piercing) {
+						public Projectile getArrow(Level level, float damage, int knockback, byte piercing) {
 							AbstractArrow entityToSpawn = new Arrow(EntityType.ARROW, level) {
 								@Override
 								public byte getPierceLevel() {
@@ -30,12 +43,11 @@ public class DiyRightclickedProcedure {
 									}
 								}
 							};
-							entityToSpawn.setOwner(shooter);
 							entityToSpawn.setBaseDamage(damage);
 							entityToSpawn.igniteForSeconds(100);
 							return entityToSpawn;
 						}
-					}.getArrow(projectileLevel, entity, 10000000, 10000, (byte) 100);
+					}.getArrow(projectileLevel, 99999, 10000, (byte) 100);
 					_entityToSpawn.setPos(_shootFrom.getX(), _shootFrom.getEyeY() - 0.1, _shootFrom.getZ());
 					_entityToSpawn.shoot(_shootFrom.getLookAngle().x, _shootFrom.getLookAngle().y, _shootFrom.getLookAngle().z, 100, 0);
 					projectileLevel.addFreshEntity(_entityToSpawn);
@@ -43,8 +55,8 @@ public class DiyRightclickedProcedure {
 			}
 		} else if (Math.random() >= 0.6) {
 			for (int index0 = 0; index0 < 20; index0++) {
-				entity.hurt(new DamageSource(world.holderOrThrow(ResourceKey.create(Registries.DAMAGE_TYPE, ResourceLocation.parse("random:deleted_mod_element")))), 10000000000000);
-				entity.hurt(new DamageSource(world.holderOrThrow(ResourceKey.create(Registries.DAMAGE_TYPE, ResourceLocation.parse("random:deleted_mod_element")))), 1);
+				entity.hurt(new DamageSource(world.holderOrThrow(ResourceKey.create(Registries.DAMAGE_TYPE, ResourceLocation.parse("random:erased_from_existence")))), 99999);
+				entity.hurt(new DamageSource(world.holderOrThrow(ResourceKey.create(Registries.DAMAGE_TYPE, ResourceLocation.parse("random:erased_from_existence")))), 1);
 			}
 		}
 	}
